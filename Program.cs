4bd97c8bd3584877d29076dd5b0811eefeeb2bbe
JASON VOLNEY DA SILVA <jason.vsilva@senacsp.edu.br>
2025-03-18 15:23:57 -0300

    incluída o novo pacote

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Microsoft.Office.Interop.Excel;
+
+string filePath = @"C:\\Users\\jason.vsilva\\Desktop\\ReadExcel\\planilha.xls";
+
+
+var excel = new Application();
+
+Workbook wb;
+Worksheet ws;
+
+wb = excel.Workbooks.Open(filePath);
+ws = (Worksheet)wb.Worksheets[1];
+
+Microsoft.Office.Interop.Excel.Range cell = (Microsoft.Office.Interop.Excel.Range)ws.Cells[3,2];
+Microsoft.Office.Interop.Excel.Range cell1 = (Microsoft.Office.Interop.Excel.Range)ws.Cells[3,2];
+
+string CellValue = (string)cell.Value;
+string Cell1Value = (string)cell1.Value;
+
+Console.WriteLine("O conteúdo da coluna é " 
++ CellValue + " e a célula é " + Cell1Value);
--- obj/Debug/net9.0/ReadExcel.AssemblyInfo.cs
+++ obj/Debug/net9.0/ReadExcel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadExcel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35cce9c4ece4b819288848ae1208de00a58a1904")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadExcel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadExcel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
